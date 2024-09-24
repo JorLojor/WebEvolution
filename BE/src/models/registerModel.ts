914fe8740fb0c2d4cdd3933537039cbd22c5f38a
JorLojor <nugas123man@gmail.com>
2024-09-25 01:05:35 +0700

    gitignore

--- BE/src/models/registerModel.ts
+++ BE/src/models/registerModel.ts
@@ -1,64 +1,69 @@
 import { DBconnection } from '../config/db';
 import jsonwebtoken from 'jsonwebtoken';
-import env from 'dotenv';env.config();
+import env from 'dotenv'; env.config();
 
 export interface Register {
     RegistrationID: number;
     Nama: string;
+    Nomor_Telfon: string;
+    Nama_Instansi: string;
+    Nama_Team: string;
+    Nomor_Induk_Mahasiswa: number;
     Email: string;
     Provinsi: string;
     Kabupaten: string;
     Password: string;
-    Confirm_Password: string;
     Pilihan_Lomba: string;
     Status_Registrasi: number;
     token: string;
 }
 
-
 export const createRegister = async (newRegister: Register): Promise<void> => {
-    const { Email, Provinsi, Kabupaten, Password, Confirm_Password, Pilihan_Lomba, Status_Registrasi, token } = newRegister;
+    const { Nama, Nomor_Telfon, Nama_Instansi, Nama_Team, Nomor_Induk_Mahasiswa, Email, Provinsi, Kabupaten, Password, Pilihan_Lomba, Status_Registrasi, token } = newRegister;
+
     await DBconnection.query(
         `INSERT INTO Register 
-            ( Email, Provinsi, Kabupaten, Password, Confirm_Password, Pilihan_Lomba, Status_Registrasi,token) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)`,
-        [Email, Provinsi, Kabupaten, Password, Confirm_Password, Pilihan_Lomba, Status_Registrasi, token]
+            (Nama, Nomor_Telfon, Nama_Instansi, Nama_Team, Nomor_Induk_Mahasiswa, Email, Provinsi, Kabupaten, Password, Pilihan_Lomba, Status_Registrasi, token) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [Nama, Nomor_Telfon, Nama_Instansi, Nama_Team, Nomor_Induk_Mahasiswa, Email, Provinsi, Kabupaten, Password, Pilihan_Lomba, Status_Registrasi, token]
     );
 
     // ngebuat table Team dengan RegistrationID yang sama dengan RegistrationID yang baru diinsert
     await DBconnection.query(
         'INSERT INTO Team (RegistrationID) VALUES (?)',
-        [newRegister.RegistrationID] //
+        [newRegister.RegistrationID]
     );
 
-    // pengondisian Pilhan_Lomba
-    if (Pilihan_Lomba === 'bisnis plan') {
+    // pengondisian Pilihan_Lomba
+    let dokumenSubstansi = '';
+    switch (Pilihan_Lomba) {
+        case 'bisnis plan':
+            dokumenSubstansi = 'bisnis plan';
+            break;
+        case 'uiux':
+            dokumenSubstansi = 'uiux';
+            break;
+        case 'web design':
+            dokumenSubstansi = 'web design';
+            break;
+        case 'poster infografis':
+            dokumenSubstansi = 'poster infografis';
+            break;
+        default:
+            dokumenSubstansi = '';
+    }
+
+    if (dokumenSubstansi) {
         await DBconnection.query(
-            'INSERT INTO Competitions (RegistrationID,Pernyataan_Origalitas,proposal) VALUES (?, ?)',
-            [newRegister.RegistrationID, 'bisnis plan']
-        );
-    }else if (Pilihan_Lomba === 'uiux') {
-        await DBconnection.query(
-            'INSERT INTO Competitions (RegistrationID, title) VALUES (?, ?)',
-            [newRegister.RegistrationID, 'uiux']
-        );
-    }else if (Pilihan_Lomba === 'web design') {
-        await DBconnection.query(
-            'INSERT INTO Competitions (RegistrationID, title) VALUES (?, ?)',
-            [newRegister.RegistrationID, 'web design']
-        );
-    }else if (Pilihan_Lomba === 'poster infografis') {
-        await DBconnection.query(
-            'INSERT INTO Competitions (RegistrationID, title) VALUES (?, ?)',
-            [newRegister.RegistrationID, 'poster infografis']
+            'INSERT INTO Competitions (RegistrationID, Pernyataan_Origalitas, Dokumen_Substansi) VALUES (?, ?, ?)',
+            [newRegister.RegistrationID, dokumenSubstansi, dokumenSubstansi]
         );
     }
-}
+};
 
 // login
 
 export const login = async (Email: string, Password: string): Promise<Register | null> => {
-  
     const [dataRegister]: any = await DBconnection.query('SELECT * FROM Register WHERE Email = ? AND Password = ?', [Email, Password]);
     if (dataRegister.length > 0) { // LOGIN NYA BERHASIL
         const token = jsonwebtoken.sign({ RegistrationID: dataRegister[0].RegistrationID }, process.env.SECRET_KEY as string);
@@ -67,16 +72,15 @@ export const login = async (Email: string, Password: string): Promise<Register |
         return dataRegister[0] as Register;
     } else {
         return null;
-    }   
-}
+    }
+};
 
-//logout
+// logout
 export const logout = async (RegistrationID: number): Promise<void> => {
     await DBconnection.query('UPDATE Register SET token = ? WHERE RegistrationID = ?', [null, RegistrationID]);
-}
+};
 
-
-//fungsi untuk  cek status registrasi yang mertun string status_registrasi ("sudah" atau "belum")
+// fungsi untuk cek status registrasi yang mertun string status_registrasi ("sudah" atau "belum")
 export const checkStatusRegistrasi = async (RegistrationID: number): Promise<string> => {
     const [dataRegister]: any = await DBconnection.query('SELECT Status_Registrasi FROM Register WHERE RegistrationID = ?', [RegistrationID]);
     if (dataRegister.length > 0) {
@@ -88,44 +92,13 @@ export const checkStatusRegistrasi = async (RegistrationID: number): Promise<str
     } else {
         return 'data tidak ditemukan';
     }
-}
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//admin
+// admin
 export const getAllRegisters = async (): Promise<Register[]> => {
     const [dataRegister] = await DBconnection.query('SELECT * FROM Register');
     return dataRegister as Register[];
-}
+};
 
 export const getSingleRegister = async (RegistrationID: number): Promise<Register | null> => {
     const [dataRegister]: any = await DBconnection.query('SELECT * FROM Register WHERE RegistrationID = ?', [RegistrationID]);
@@ -134,30 +107,29 @@ export const getSingleRegister = async (RegistrationID: number): Promise<Registe
     } else {
         return null;
     }
-}
-
-
-
-
+};
 
 export const updateRegister = async (RegistrationID: number, updatedRegister: Partial<Register>): Promise<void> => {
-    const { Nama, Email, Provinsi, Kabupaten, Password, Confirm_Password, Pilihan_Lomba, Status_Registrasi } = updatedRegister;
+    const { Nama, Nomor_Telfon, Nama_Instansi, Nama_Team, Nomor_Induk_Mahasiswa, Email, Provinsi, Kabupaten, Password, Pilihan_Lomba, Status_Registrasi } = updatedRegister;
 
     await DBconnection.query(
         `UPDATE Register SET 
     Nama = COALESCE(?, Nama),
+    Nomor_Telfon = COALESCE(?, Nomor_Telfon),
+    Nama_Instansi = COALESCE(?, Nama_Instansi),
+    Nama_Team = COALESCE(?, Nama_Team),
+    Nomor_Induk_Mahasiswa = COALESCE(?, Nomor_Induk_Mahasiswa),
     Email = COALESCE(?, Email),
     Provinsi = COALESCE(?, Provinsi),
     Kabupaten = COALESCE(?, Kabupaten),
     Password = COALESCE(?, Password),
-    Confirm_Password = COALESCE(?, Confirm_Password),
     Pilihan_Lomba = COALESCE(?, Pilihan_Lomba),
     Status_Registrasi = COALESCE(?, Status_Registrasi)
     WHERE RegistrationID = ?`,
-        [Nama, Email, Provinsi, Kabupaten, Password, Confirm_Password, Pilihan_Lomba, Status_Registrasi, RegistrationID]
+        [Nama, Nomor_Telfon, Nama_Instansi, Nama_Team, Nomor_Induk_Mahasiswa, Email, Provinsi, Kabupaten, Password, Pilihan_Lomba, Status_Registrasi, RegistrationID]
     );
-}
+};
 
 export const deleteRegister = async (RegistrationID: number): Promise<void> => {
     await DBconnection.query('DELETE FROM Register WHERE RegistrationID = ?', [RegistrationID]);
-}
+};
