28ef435095ad81a480d7ae3ad4242d1ad18bc3f4
JorLojor <nugas123man@gmail.com>
2024-09-25 19:47:33 +0700

    login FE

--- FE/src/auth/Login.jsx
+++ FE/src/auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
           return re.test(String(email).toLowerCase());
      };
 
-     const handleLogin = () => {
+     const handleLogin = async () => {
           if (email === "" || password === "") {
                setError("Email dan Password harus diisi.");
                return;
@@ -30,11 +30,31 @@ const Login = () => {
                return;
           }
 
-          if (email === "admin@admin.com" && password === "admin123") {
-               dispatch(login({ email, role: "admin" }));
-               navigate("/dashboard");
-          } else {
-               setError("Email atau Password salah. Silakan coba lagi.");
+          try {
+               const response = await fetch(
+                    "http://localhost:3987/api/register/login",
+                    {
+                         method: "POST",
+                         headers: {
+                              "Content-Type": "application/json",
+                         },
+                         body: JSON.stringify({ email, password }),
+                    }
+               );
+
+               const data = await response.json();
+
+               if (response.status === 200) {
+                    dispatch(login({ token: data.data }));
+                    alert("Login berhasil!");
+
+                    navigate("/dashboard");
+               } else {
+                    setError(data.message || "Email atau Password salah.");
+               }
+          } catch (error) {
+               console.error(error);
+               setError("Terjadi kesalahan saat login.");
           }
      };
 
@@ -44,7 +64,7 @@ const Login = () => {
 
      return (
           <Fragment>
-               <div className="flex items-center ">
+               <div className="flex items-center">
                     <div
                          className={`${
                               isMobile ? "hidden" : "w-1/2"
@@ -86,97 +106,40 @@ const Login = () => {
                                    />
                               </div>
                          </div>
-                         <div
-                              className={`${
-                                   isMobile
-                                        ? "w-screen px-4 flex flex-col justify-center items-center min-h-screen"
-                                        : "w-1/2"
-                              }`}>
-                              <h1 className="text-center text-3xl font-semibold">
-                                   Selamat Datang
-                              </h1>
-                              <h1 className="text-center text-lg">
-                                   Silahkan masukan detail akun anda untuk
-                                   mengakses dashboard
-                              </h1>
-                              <div className="flex justify-center items-center mt-6 w-full">
-                                   <div
-                                        className={`flex items-center h-[80px] ${
-                                             isMobile ? "w-[90%]" : "w-[80%]"
-                                        } border-2 border-gray-300 rounded-[10px] relative`}>
-                                        <FaUser className="ml-4" />
-                                        <input
-                                             className={`h-full w-full pl-4 border-none outline-none text-[20px] font-normal ${
-                                                  id
-                                                       ? "text-black"
-                                                       : "text-[#616161]"
-                                             }`}
-                                             type="text"
-                                             placeholder="ID"
-                                             value={id}
-                                             onChange={(e) =>
-                                                  setId(e.target.value)
-                                             }
-                                             required
-                                        />
-                                   </div>
-                              </div>
-                              <div className="flex justify-center items-center mt-6 w-full">
-                                   <div
-                                        className={`flex items-center h-[80px] ${
-                                             isMobile ? "w-[90%]" : "w-[80%]"
-                                        } border-2 border-gray-300 rounded-[10px] relative`}>
-                                        <FaLock className="ml-4" />
-                                        <input
-                                             className={`h-full w-full pl-4 pr-12 border-none outline-none text-[20px] font-normal ${
-                                                  password
-                                                       ? "text-black"
-                                                       : "text-[#616161]"
-                                             }`}
-                                             type={
-                                                  showPassword
-                                                       ? "text"
-                                                       : "password"
-                                             }
-                                             placeholder="Password"
-                                             value={password}
-                                             onChange={(e) =>
-                                                  setPassword(e.target.value)
-                                             }
-                                             required
-                                        />
-                                        <button
-                                             className="absolute right-4 top-1/2 transform -translate-y-1/2 focus:outline-none"
-                                             onClick={() =>
-                                                  setShowPassword(!showPassword)
-                                             }>
-                                             {showPassword ? (
-                                                  <FaEyeSlash size={20} />
-                                             ) : (
-                                                  <FaEye size={20} />
-                                             )}
-                                        </button>
-                                   </div>
-                              </div>
-                              <div className="w-full">
+                         <div className="flex justify-center items-center mt-6 w-full">
+                              <div
+                                   className={`flex items-center h-[80px] ${
+                                        isMobile ? "w-[90%]" : "w-[80%]"
+                                   } border-2 border-gray-300 rounded-[10px] relative`}>
+                                   <FaLock className="ml-4" />
+                                   <input
+                                        className={`h-full w-full pl-4 pr-12 border-none outline-none text-[20px] font-normal ${
+                                             password
+                                                  ? "text-black"
+                                                  : "text-[#616161]"
+                                        }`}
+                                        type={
+                                             showPassword ? "text" : "password"
+                                        }
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={(e) =>
+                                             setPassword(e.target.value)
+                                        }
+                                        required
+                                   />
                                    <button
-                                        className={`${
-                                             password && id
-                                                  ? "bg-[black]"
-                                                  : "bg-[#616161]"
-                                        }  text-white h-[87px] w-[80%] ml-[10%] rounded-[10px] mt-7 text-[20px] font-medium`}
-                                        onClick={handleLogin}>
-                                        Masuk
+                                        className="absolute right-4 top-1/2 transform -translate-y-1/2 focus:outline-none"
+                                        onClick={() =>
+                                             setShowPassword(!showPassword)
+                                        }>
+                                        {showPassword ? (
+                                             <FaEyeSlash size={20} />
+                                        ) : (
+                                             <FaEye size={20} />
+                                        )}
                                    </button>
                               </div>
-                              <p className="text-center mt-4">
-                                   Belum Mempunyai Akun?{" "}
-                                   <span
-                                        className="text-blue-500"
-                                        onClick={(e) => navigate("/register")}>
-                                        Daftar
-                                   </span>
-                              </p>
                          </div>
                          <div className="w-full">
                               <button
@@ -184,7 +147,7 @@ const Login = () => {
                                         password && email
                                              ? "bg-[black]"
                                              : "bg-[#616161]"
-                                   }  text-white h-[87px] w-[80%] ml-[10%] rounded-[10px] mt-7 text-[20px] font-medium`}
+                                   } text-white h-[87px] w-[80%] ml-[10%] rounded-[10px] mt-7 text-[20px] font-medium`}
                                    onClick={handleLogin}>
                                    Masuk
                               </button>
