46fe84fb904f5d954dfa5c679b654f6374930c8e
JorLojor <nugas123man@gmail.com>
2024-09-27 13:42:42 +0700

    team features

--- FE/src/pages/MemberTeam.jsx
+++ FE/src/pages/MemberTeam.jsx
@@ -13,6 +13,69 @@ const MemberTeam = () => {
           NIM_Anggota3: "",
      });
 
+     // endpoint http://localhost:3987/api/team/my-team
+
+     // response
+     //  {
+     //     "TeamID": 567,
+     //     "RegistrationID": 839,
+     //     "Nama_Anggota1": "jors",
+     //     "NIM_Anggota1": 1303213147,
+     //     "Nama_Anggota2": "abang",
+     //     "NIM_Anggota2": 123456,
+     //     "Nama_Anggota3": "adek",
+     //     "NIM_Anggota3": 654321
+     // }
+
+     const [teamData, setTeamData] = useState({
+          Nama_Anggota1: "",
+          NIM_Anggota1: 0,
+          Nama_Anggota2: "",
+          NIM_Anggota2: 0,
+          Nama_Anggota3: "",
+          NIM_Anggota3: 0,
+     });
+     const handleShowMemberTeam = async () => {
+          if (!tokenne) {
+               console.error("Token tidak tersedia, silakan login kembali.");
+               return;
+          }
+
+          try {
+               const response = await fetch(
+                    "http://localhost:3987/api/team/my-team",
+                    {
+                         method: "GET",
+                         headers: {
+                              Authorization: `Bearer ${tokenne}`,
+                         },
+                    }
+               );
+
+               const result = await response.json();
+
+               if (response.ok) {
+                    console.log("Team members retrieved successfully:", result);
+                    setTeamData({
+                         Nama_Anggota1: result.Nama_Anggota1,
+                         NIM_Anggota1: result.NIM_Anggota1,
+                         Nama_Anggota2: result.Nama_Anggota2,
+                         NIM_Anggota2: result.NIM_Anggota2,
+                         Nama_Anggota3: result.Nama_Anggota3,
+                         NIM_Anggota3: result.NIM_Anggota3,
+                    });
+               } else {
+                    console.error(
+                         "Error retrieving team members:",
+                         result.message
+                    );
+                    alert("Gagal menampilkan anggota tim: " + result.message);
+               }
+          } catch (error) {
+               console.error("Error during member team request:", error);
+          }
+     };
+
      const handleChange = (e) => {
           setFormData({
                ...formData,
